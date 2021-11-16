916132d42d3cfa9ceb632c37edd93265f8a824f8
garrysen <garrysen.christian@gmail.com>
2021-11-17 01:11:40 +0800

    init

---
+++ pdo.php
@@ -0,0 +1,10 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=u853730548_enamitc2',
+   'u853730548_itc2admin', ':U5tjP&e;4]');
+// See the "errors" folder for details...
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($pdo -> connect_error){
+  echo "Database is not online";
+  exit;
+}
