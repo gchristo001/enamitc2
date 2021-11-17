0933db5322c57e361017191d91596ed2465697ad
gchristo001 <gabrian.christopher@gmail.com>
2021-11-17 14:24:05 +0800

    update js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let searchForm = document.querySelector('.header .search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
 }
 
 let slides = document.querySelectorAll('.home .slide');
