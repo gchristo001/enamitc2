c552aa74e3923bae4c8af05a7b48f92c8549bab8
gchristo001 <gabrian.christopher@gmail.com>
2021-11-17 12:52:37 +0800

    revert

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ $randomitem = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 </head>
+<body>
 
 <!-- header section starts  -->
 
@@ -93,7 +94,7 @@ $randomitem = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!-- header section ends -->
 
 <!-- home section starts      -->
-<body>
+
 <section class="home">
 
     <div class="slide active" style="background: url(images/banner1.jpg) no-repeat;">
