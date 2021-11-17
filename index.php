fc9987b1aee19a1bb09efe6b10b2c706f3e89faf
garrysen <garrysen.christian@gmail.com>
2021-11-17 22:23:19 +0800

    construction\

--- index.php
+++ index.php
@@ -57,64 +57,6 @@ $randomitem = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <a href="index.php" class="logo"> <img src="images/Logo.png"> </a>
 
-    <nav class="navbar">
-        <ul>
-            <li><a href="index.php">HOME</a></li>
-            <li><a>SHOP CATEGORIES</a>                
-                <ul>
-                    <li><a href = "product_list.php?category=Necklace">Necklace</a></li>
-                    <li><a href = "product_list.php?category=Bangle">Bangle</a></li>
-                    <li><a href = "product_list.php?category=Bracelet">Bracelet</a></li>
-                    <li><a href = "product_list.php?category=Ring">Ring</a></li>
-                    <li><a href = "product_list.php?category=Earings">Earings</a></li>
-                    <li><a href = "product_list.php?category=Pendant">Pendant</a></li>
-                    <li><a href = "product_list.php?category=Kids">Kids</a></li>
-                    <li><a href = "product_list.php?category=Dubai gold">Dubai</a></li>
-                    <li><a href = "product_list.php?category=Gold bar">Gold bar</a></li>
-                </ul>
-            </li>
-            <li><a>COLLECTION</a>
-                <ul>
-                    <li><a href = "product_list.php?supplier=DeGold">DeGold</a></li>
-                    <li><a href = "product_list.php?supplier=UBS">UBS</a></li>
-                    <li><a href = "product_list.php?supplier=Citra">Citra</a></li>
-                    <li><a href = "product_list.php?supplier=Lotus">Lotus</a></li>
-                    <li><a href = "product_list.php?supplier=YT">YT</a></li>
-                    <li><a href = "product_list.php?supplier=Kinghalim">Kinghalim</a></li>
-                    <li><a href = "product_list.php?supplier=HWT">HWT</a></li>
-                    <li><a href = "product_list.php?supplier=Bulgari">Bulgari</a></li>
-                    <li><a href = "product_list.php?supplier=Ayu">Ayu</a></li>
-                    <li><a href = "product_list.php?supplier=SJW">SJW</a></li>
-                    <li><a href = "product_list.php?supplier=Hala">Hala</a></li>
-                    <li><a href = "product_list.php?supplier=Amero">Amero</a></li>
-                    <li><a href = "product_list.php?supplier=MT">MT</a></li>
-                </ul>
-            </li>
-            <li><a href="#footer">ABOUT US</a></li>
-        </ul>
-    </nav>
-
-    <div class="icons">
-        <a href="product_list.php" id="shop-btn" class="fas fa-store"></a>
-        <div id="search-btn" class="fas fa-search"></div>
-        <a href="cart.php" class="fas fa-shopping-cart"></a>
-        <span class="badge"><?=$badge?></span>
-        <a href="
-                <?php
-                if (isset($_SESSION['userid'])){
-                  echo 'profile.php';
-                }
-                else {
-                  echo 'login.php';
-                }
-                ?>" class="fas fa-user"></a>
-    </div>
-
-    <form action="" class="search-form">
-        <input type="search" name="" placeholder="search here..." id="search-box">
-        <label for="search-box" class="fas fa-search"></label>
-    </form>
-
 </header>
 <!-- header section ends -->
 
@@ -124,75 +66,20 @@ $randomitem = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <div class="slide active" style="background: url(images/banner1.jpg) no-repeat;">
         <div class="content">
-            <span>Celebrate Your Birth Month</span>
-            <h3>Birth Stone</h3>
-            <a href="#" class="btn">shop now</a>
+            <span>SEDANG DALAM KONSTRUKSI</span>
+            <h3>Ditunggu ya, bentar lagi ada yang seru!~~</h3>
         </div>
     </div>
-
-    <div class="slide" style="background: url(images/banner2.jpg) no-repeat;">
-        <div class="content">
-            <span>Beutify Yourself</span>
-            <h3>DeGold Collection</h3>
-            <a href="#" class="btn">shop now</a>
-        </div>
-    </div>
-
-    <div class="slide" style="background: url(images/banner66.jpg) no-repeat;">
-        <div class="content">
-            <span>Promotion</span>
-            <h3>upto 50% off</h3>
-            <a href="#" class="btn">shop now</a>
-        </div>
-    </div>
-
-    <div id="next-slide" onclick="next()" class="fas fa-angle-right"></div>
-    <div id="prev-slide" onclick="prev()" class="fas fa-angle-left"></div>
 
 </section>
 
 
 <!-- menu section ends -->
-
-
 <!-- footer section starts  -->
 
 <section class="footer">
 
     <div class="box-container">
-
-        <div class="box">
-            <h3>Shop Categories</h3>
-            <a href = "product_list.php?category=Necklace"><i class="fas fa-angle-right"></i>Necklace</a>
-            <a href = "product_list.php?category=Bangle"><i class="fas fa-angle-right"></i>Bangle</a>
-            <a href = "product_list.php?category=Bracelet"><i class="fas fa-angle-right"></i>Bracelet</a>
-            <a href = "product_list.php?category=Ring"><i class="fas fa-angle-right"></i>Ring</a>
-            <a href = "product_list.php?category=Earings"><i class="fas fa-angle-right"></i>Earings</a>
-            <a href = "product_list.php?category=Pendant"><i class="fas fa-angle-right"></i>Pendant</a>
-            <a href = "product_list.php?category=Kids"><i class="fas fa-angle-right"></i>Kids</a>
-            <a href = "product_list.php?category=Dubai gold"><i class="fas fa-angle-right"></i>Dubai</a>
-            <a href = "product_list.php?category=Gold bar"><i class="fas fa-angle-right"></i>Gold bar</a>
-        </div>
-
-        <div class="box">
-            <h3>Collection</h3>
-                <div class="footer-link">
-                <a href = "product_list.php?supplier=DeGold"><i class="fas fa-angle-right"></i>DeGold</a>
-                <a href = "product_list.php?supplier=UBS"><i class="fas fa-angle-right"></i>UBS</a>
-                <a href = "product_list.php?supplier=Citra"><i class="fas fa-angle-right"></i>Citra</a>
-                <a href = "product_list.php?supplier=Lotus"><i class="fas fa-angle-right"></i>Lotus</a>
-                <a href = "product_list.php?supplier=YT"><i class="fas fa-angle-right"></i>YT</a>
-                <a href = "product_list.php?supplier=Kinghalim"><i class="fas fa-angle-right"></i>Kinghalim</a>
-                <a href = "product_list.php?supplier=HWT"><i class="fas fa-angle-right"></i>HWT</a>
-                <a href = "product_list.php?supplier=Bulgari"><i class="fas fa-angle-right"></i>Bulgari</a>
-                <a href = "product_list.php?supplier=Ayu"><i class="fas fa-angle-right"></i>Ayu</a>
-                <a href = "product_list.php?supplier=SJW"><i class="fas fa-angle-right"></i>SJW</a>
-                <a href = "product_list.php?supplier=Hala"><i class="fas fa-angle-right"></i>Hala</a>
-                <a href = "product_list.php?supplier=Amero"><i class="fas fa-angle-right"></i>Amero</a>
-                <a href = "product_list.php?supplier=MT"><i class="fas fa-angle-right"></i>MT</a>
-                </div>
-        </div>
-
         <div class="box">
             <h3>follow us</h3>
             <a href="https://en-gb.facebook.com/tokomasenamitc2/?ref=page_internal"> <i class="fab fa-facebook-f"></i> facebook </a>
@@ -217,15 +104,12 @@ $randomitem = $stmt->fetchAll(PDO::FETCH_ASSOC);
            <p><i class="fas fa-map-marker-alt"></i>  itc kebon kalapa lt. dasar blok a2 no 7,8,9,16 </p>
            <p><i class="far fa-clock"></i>  Monday - Saturday 09:00 - 16:00 </p>
         </div>
-
     </div>
 
     <div class="credit"> created by <span>GC</span> | all rights reserved </div>
 
 </section>
 <!-- footer section ends -->
-
-
 
 <!-- custom js file link -->
 <script src="script.js"></script>
