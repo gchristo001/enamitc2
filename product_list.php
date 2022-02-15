1c2e57e1a9301408cbdc14768dfb64ed3a2540fd
gchristo001 <gabrian.christopher@gmail.com>
2022-02-15 21:16:39 +0800

    modal images

--- product_list.php
+++ product_list.php
@@ -215,6 +215,87 @@ $badge = count($_SESSION['cart']);
             width: 15rem;
             }
         }
+
+           /* Style the Image Used to Trigger the Modal */
+        #myImg {
+        border-radius: 5px;
+        cursor: pointer;
+        transition: 0.3s;
+        }
+
+        #myImg:hover {opacity: 0.7;}
+
+        /* The Modal (background) */
+        .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        padding-top: 200px; /* Location of the box */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+        }
+
+        /* Modal Content (Image) */
+        .modal-content {
+        margin: auto;
+        display: block;
+        width: 80%;
+        max-width: 700px;
+        }
+
+        /* Caption of Modal Image (Image Text) - Same Width as the Image */
+        #caption {
+        margin: auto;
+        display: block;
+        width: 80%;
+        max-width: 700px;
+        text-align: center;
+        color: #ccc;
+        padding: 10px 0;
+        height: 150px;
+        }
+
+        /* Add Animation - Zoom in the Modal */
+        .modal-content, #caption {
+        animation-name: zoom;
+        animation-duration: 0.6s;
+        }
+
+        @keyframes zoom {
+        from {transform:scale(0)}
+        to {transform:scale(1)}
+        }
+
+        /* The Close Button */
+        .close {
+        position: absolute;
+        top: 100px;
+        right: 20px;
+        color: #f1f1f1;
+        font-size: 80px;
+        font-weight: bold;
+        transition: 0.3s;
+        }
+
+        .close:hover,
+        .close:focus {
+        color: #bbb;
+        text-decoration: none;
+        cursor: pointer;
+        }
+
+        /* 100% Image Width on Smaller Screens */
+        @media only screen and (max-width: 700px){
+        .modal-content {
+            width: 100%;
+        }
+        }
+
     </style>
 
     <script>
@@ -361,7 +442,7 @@ $badge = count($_SESSION['cart']);
     
         foreach ( $displayitems as $item ) {
             echo("<div class=\"box\">");
-            echo("<img src=\"item-image/".($item['image'])." \">");
+            echo("<img class=\"myImages\" id=\"myImg\" src=\"item-image/".($item['image'])." \">");
             echo("<h3>".$item['name']."</h3>");
             echo("<div class=\"weight-size\">".$item['weight']." gr");
             if($item['size']>0){
@@ -383,7 +464,7 @@ $badge = count($_SESSION['cart']);
         if($_GET['category'] == "Sold out"){
             foreach ( $soldout as $item ) {
                 echo("<div class=\"box\">");
-                echo("<img src=\"item-image/".($item['image'])." \" style=\"opacity: 0.6;
+                echo("<img class=\"myImages\" id=\"myImg\" src=\"item-image/".($item['image'])." \" style=\"opacity: 0.6;
                 filter: alpha(opacity=60);\">");
                 echo("<h3>".$item['name']."</h3>");
                 echo("<div class=\"weight-size\">".$item['weight']." gr");
@@ -406,6 +487,48 @@ $badge = count($_SESSION['cart']);
 </section>
 
 <!-- menu section ends -->
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+<!-- The Close Button -->
+<span class="close">&times;</span>
+
+<!-- Modal Content (The Image) -->
+<img class="modal-content" id="img01">
+
+<!-- Modal Caption (Image Text) -->
+<div id="caption"></div>
+
+</div>
+
+<script>
+   // create references to the modal...
+    var modal = document.getElementById('myModal');
+    // to all images -- note I'm using a class!
+    var images = document.getElementsByClassName('myImages');
+    // the image in the modal
+    var modalImg = document.getElementById("img01");
+    // and the caption in the modal
+    var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+</script>
 
 
 
