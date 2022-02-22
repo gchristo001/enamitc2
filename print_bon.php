59f727dd1089c690876ea951ba77774762b63b1c
gchristo001 <gabrian.christopher@gmail.com>
2022-02-22 21:22:12 +0800

    printbon

--- print_bon.php
+++ print_bon.php
@@ -1,5 +1,41 @@
 <?php 
+require_once "pdo.php";
+session_start();
 
+
+
+
+if(isset($_GET['orderid'])){
+    $sql = 
+        " SELECT 
+        orders.orderid as orderid,
+        orders.orderdate,
+        orders.userid,
+        orders.admin,
+        users.username,
+        users.phone,
+        users.address,
+        item_attributes.attributeid as attributeid,
+        item_attributes.size as size,
+        item_attributes.weight as weight,
+        item_attributes.price as price,
+        items.code as code,
+        items.name,
+        items.image
+        FROM orders
+        LEFT JOIN users
+        ON orders.userid = users.userid 
+        LEFT JOIN item_attributes
+        ON orders.attributeid = item_attributes.attributeid
+        LEFT JOIN items
+        ON item_attributes.itemid = items.itemid
+        WHERE orderid = :orderid 
+        ";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(
+            ":orderid" => $_GET['orderid']  ));
+        $print = $stmt->fetch(PDO::FETCH_ASSOC);
+}
 
 
 ?>
@@ -17,12 +53,22 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="print.css?<?=filemtime('admin.css');?>">
+    <link rel="stylesheet" href="print.css?<?=filemtime('print.css');?>">
 
 </head>
 
 <body>
 
+<div class = "printableArea">
+    <textarea id = "userid"> <?= $print['userid'] ?> </textarea>
+    <textarea id = "nama"> <?= $print['username'] ?> </textarea>
+    <textarea id = "tanggal"> <?= $print['orderdate'] ?> </textarea>
+    <textarea id = "berat"> <?= $print['weight'] ?> </textarea>
+    <textarea id = "size"> <?= $print['size'] ?> </textarea>
+    <textarea id = "kode"> <?= $print['code'] ?> </textarea>
+    <textarea id = "nama_barang"> <?= $print['name'] ?> </textarea>
+    <img id = "gambar" src = "item-image/<?= $print['image'] ?>"> 
+</div>
 
 </body>
 
