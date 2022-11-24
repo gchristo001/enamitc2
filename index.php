1fe5df9386d631664743e2268f8204d2296cc74d
garrysen <garrysen.christian@gmail.com>
2022-11-24 23:04:17 +0800

    asdf

--- index.php
+++ index.php
@@ -330,10 +330,8 @@ $badge = count($_SESSION['cart']);
 
 <!-- home section ends     -->
 
-<!-- category section starts  -->
-<div class="video"> 
-        <iframe  width="900" height="720" src="https://www.youtube.com/embed/rcSxMPKSQuU" title="Lavani by Amero Jewellery x Purana" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
+<!-- category section starts  --> 
+<iframe  display="flex" src="https://www.youtube.com/embed/rcSxMPKSQuU" title="Lavani by Amero Jewellery x Purana" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 <section class="category">
 
